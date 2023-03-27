8448d98771040dd1abb73930a16edd13320fbd20
Motiche <mot.hakimi.95@gmail.com>
2023-03-27 21:15:48 +0430

    Integrated SheCodes API

--- src/index.js
+++ src/index.js
@@ -7,23 +7,6 @@ let Week = [
   "Friday",
   "Saturday",
 ];
-var Icon_dict = {
-  Clear: "☀️",
-  Clouds: "⛅",
-  Drizzle: "🌧️",
-  Rain: "🌦️",
-  Thunderstorm: "⛈️",
-  Snow: "❄️",
-  Mist: "🌫️",
-  Smoke: "🌫️",
-  Haze: "🌫️",
-  Dust: "🌫️",
-  Fog: "🌁",
-  Sand: "⏳",
-  Ash: "🌫️",
-  Squall: "💨",
-  Tornado: "🌪️",
-};
 
 /// Celcius to Farenheit
 function C_to_F(temp) {
@@ -37,10 +20,16 @@ function C_to_F_convert() {
   for (let t = 0; t < Ts.length; t++) {
     let T = Ts[t];
     let tem = T.innerHTML;
-    tem = tem.substring(0, tem.length - 2);
-    tem = Number(tem);
-    tem = C_to_F(tem);
-    T.innerHTML = `${tem}°F`;
+    if (tem.length > 2) {
+      tem = tem.substring(0, tem.length - 2);
+      tem = Number(tem);
+      tem = C_to_F(tem);
+      T.innerHTML = `${tem}°F`;
+    } else {
+      tem = Number(tem);
+      tem = C_to_F(tem);
+      T.innerHTML = `${tem}`;
+    }
   }
 }
 
@@ -59,10 +48,16 @@ function F_to_C_convert() {
   for (let t = 0; t < Ts.length; t++) {
     let T = Ts[t];
     let tem = T.innerHTML;
-    tem = tem.substring(0, tem.length - 2);
-    tem = Number(tem);
-    tem = F_to_C(tem);
-    T.innerHTML = `${tem}°C`;
+    if (tem.length > 2) {
+      tem = tem.substring(0, tem.length - 2);
+      tem = Number(tem);
+      tem = F_to_C(tem);
+      T.innerHTML = `${tem}°C`;
+    } else {
+      tem = Number(tem);
+      tem = F_to_C(tem);
+      T.innerHTML = `${tem}`;
+    }
   }
 }
 document.getElementById("C_button").disabled = true;
@@ -71,8 +66,7 @@ document.getElementById("C_button").onclick = function () {
 };
 
 // API key
-let API_key = "b1c040ca4f95f4c9b373d01b21c7e668";
-
+let API_key = "4t46b93oa4b0a2872a4342a90af06e55";
 let day = "";
 //
 async function fixTime(time_url) {
@@ -80,7 +74,8 @@ async function fixTime(time_url) {
   var data = await response.json();
   // console.log(data);
   let now = new Date(data.date_time_txt);
-  day = Week[now.getDay()];
+  day = now.getDay();
+  day_now = Week[day];
   let Hour = now.getHours();
   if (Hour < 10) {
     Hour = `0${Hour}`;
@@ -90,9 +85,12 @@ async function fixTime(time_url) {
     Minutes = `0${Minutes}`;
   }
 
-  let Time_text = `${day}, ${Hour}:${Minutes}`;
+  let Time_text = `${day_now}, ${Hour}:${Minutes}`;
   let Time = document.getElementById("selected_city");
   Time.innerHTML = Time_text;
+  V1 = this.Weekdays.slice(day, 7);
+  V2 = this.Weekdays.slice(0, day);
+  Weekdays_reorder = V1.concat(V2);
 }
 // 🕵️‍♀️Feature #2
 // Add a search engine, when searching for a city (i.e. Paris), display the city name on the page after the user submits the form.
@@ -103,29 +101,33 @@ String.prototype.toProperCase = function () {
 };
 
 function ShowTemp(response) {
-  let Temp = response.data.main;
-  // console.log(response);
-  document.getElementById("Temp-now").innerHTML = `${Math.round(Temp.temp)}°C`;
+  console.log(response);
+  let Temp = response.data.temperature;
+
+  document.getElementById("Temp-now").innerHTML = `${Math.round(
+    Temp.current
+  )}°C`;
   document.getElementById("feel").innerHTML = `${Math.round(
     Temp.feels_like
   )}°C`;
   document.getElementById("humidity").innerHTML = `${Temp.humidity}%`;
-  let looks = response.data.weather[0].description;
+  let looks = response.data.condition.description;
   document.getElementById("Looks").innerHTML = `${looks.toProperCase()}`;
   document.getElementById(
     "wind"
   ).innerHTML = `${response.data.wind.speed} km/h`;
-  document.querySelector("#City").innerHTML = response.data.name;
-  let Icon = response.data.weather[0].main;
-  document.getElementById("Weather-icon").innerHTML = Icon_dict[Icon];
+  document.querySelector("#City").innerHTML = response.data.city;
+  Coords.lat = response.data.coordinates.latitude;
+  Coords.lon = response.data.coordinates.longitude;
   let time_API_Key = "b0e84ce4817c454fa7ef006f3a33df4f";
-  let time_API = `https://api.ipgeolocation.io/timezone?apiKey=${time_API_Key}&lat=${response.data.coord.lat}&long=${response.data.coord.lon}`;
+  let time_API = `https://api.ipgeolocation.io/timezone?apiKey=${time_API_Key}&lat=${Coords.lat}&long=${Coords.lon}`;
   fixTime(time_API);
+  Forcast_Weather();
 }
 function Show_city() {
   let input = document.querySelector("#Input-city");
   let new_city = input.value.toProperCase();
-  let Weather_City_URL = `https://api.openweathermap.org/data/2.5/weather?q=${new_city}&appid=${API_key}&units=metric`;
+  let Weather_City_URL = `https://api.shecodes.io/weather/v1/current?query=${new_city}&key=${API_key}`;
   axios.get(Weather_City_URL).then(ShowTemp);
 }
 // Search city button
@@ -153,66 +155,96 @@ navigator.geolocation.getCurrentPosition(handlePosition);
 // change temp according to coord
 function Coord_Temp(event) {
   event.preventDefault();
-  let Weather_City_URL = `https://api.openweathermap.org/data/2.5/weather?lat=${Coords.lat}&lon=${Coords.lon}&appid=${API_key}&units=metric`;
-  console.log(Weather_City_URL);
+  let Weather_City_URL = `https://api.shecodes.io/weather/v1/current?lon=${Coords.lon}&lat=${Coords.lat}&key=${API_key}&units=metric`;
   axios.get(Weather_City_URL).then(ShowTemp);
 }
 
 document.querySelector("#My-coords").addEventListener("click", Coord_Temp);
+let TEMP = 0;
+// Forcast Weather
+function Forcast_Weather_data(response) {
+  TEMP = response.data.daily;
+  console.log(TEMP);
+  for (let i = 0; i < 7; i++) {
+    Temp = TEMP[i].temperature;
+    Temp_forcast[i] = Math.round(Temp.day);
+    Temp_min[i] = Math.round(Temp.minimum);
+    Temp_max[i] = Math.round(Temp.maximum);
+    Humidity_forcast[i] = Temp.humidity;
+    Wind_forcast[i] = TEMP[i].wind.speed;
+    Icons[i] = TEMP[i].condition.icon_url;
+  }
+  Show_forcast();
+}
 
-// for tamplate
-let Weather_City_URL = `https://api.openweathermap.org/data/2.5/weather?q=Tehran&appid=${API_key}&units=metric`;
-axios.get(Weather_City_URL).then(ShowTemp);
-
-let day_index = Week.find((element) => element === day);
-console.log(day_index);
-
-let Weekdays = ["Sun", "Mon", "Tue", "Wed", "Thr", "Fri", "Sat"];
-let Temp_forcast = [23, 21, 24, 25, 27, 23, 21];
-let Wind_forcast = [5, 4, 6, 5.5, 3, 2, 4];
-let Humidity_forcast = [31, 34, 25, 41, 38, 24, 27];
+// Forcast_Weather
+function Forcast_Weather() {
+  let Forcast_URL = `https://api.shecodes.io/weather/v1/forecast?lat=${Coords.lat}&lon=${Coords.lon}&key=${API_key}&units=metric`;
+  console.log(Forcast_URL);
+  axios.get(Forcast_URL).then(Forcast_Weather_data);
+}
 
 /// Week Plot
-
-new Chart("myChart", {
-  type: "line",
-  data: {
-    labels: Weekdays,
-    datasets: [
-      {
-        label: "Tempreture (°C)",
-        data: Temp_forcast,
-        borderColor: "rgb(255, 111, 111)",
-        fill: false,
-      },
-      {
-        label: "Wind Speed (km/h)",
-        data: Wind_forcast,
-        borderColor: "rgb(83, 198, 114)",
-        fill: false,
-      },
-      {
-        label: "Humidity (%)",
-        data: Humidity_forcast,
-        borderColor: "rgb(54, 162, 200)",
-        fill: false,
-      },
-    ],
-  },
-  options: {
-    legend: { display: true },
-  },
-});
+function plot() {
+  new Chart("myChart", {
+    type: "line",
+    data: {
+      labels: Weekdays_reorder,
+      datasets: [
+        {
+          label: "Tempreture (°C)",
+          data: Temp_forcast,
+          borderColor: "rgb(255, 111, 111)",
+          fill: false,
+        },
+        {
+          label: "Wind Speed (km/h)",
+          data: Wind_forcast,
+          borderColor: "rgb(83, 198, 114)",
+          fill: false,
+        },
+        {
+          label: "Humidity (%)",
+          data: Humidity_forcast,
+          borderColor: "rgb(54, 162, 200)",
+          fill: false,
+        },
+      ],
+    },
+    options: {
+      legend: { display: true },
+    },
+  });
+}
 
 function Show_forcast() {
   let Forcast_html = ``;
   for (let i = 1; i < 7; i++) {
     Forcast_html += `<div class="col-md-2 card card-body Weather-card">
-          <h2>${Weekdays[i]}</h2>
-          <p>☁️ <span class="Temp">${Temp_forcast[i]}°C</span></p>
+          <p> <b>${Weekdays_reorder[i]} </b><span class="Temp Prediction">${Temp_min[i]}</span> - 
+          <span class="Temp Prediction">${Temp_max[i]}°C</span>
+          <img src=${Icons[i]}></p>
+          
         </div>`;
   }
+  document.getElementById("Weather-icon").innerHTML = `<img src = ${Icons[0]} 
+  width="150" height="150">`;
   document.getElementById("forcast-cards").innerHTML = Forcast_html;
+  plot();
 }
 
-Show_forcast();
+// for tamplate
+let Weather_City_URL = `https://api.shecodes.io/weather/v1/current?query=Tehran&key=${API_key}`;
+axios.get(Weather_City_URL).then(ShowTemp);
+
+var Weekdays = ["Sun", "Mon", "Tue", "Wed", "Thr", "Fri", "Sat"];
+console.log(day);
+var V1 = this.Weekdays.slice(day, 7);
+var V2 = this.Weekdays.slice(0, day);
+var Weekdays_reorder = V1.concat(V2);
+var Temp_forcast = [23, 24, 22, 21, 23, 24, 25];
+var Temp_min = [21, 21, 20, 19, 22, 23, 24];
+var Temp_max = [23, 24, 22, 21, 23, 24, 25];
+var Wind_forcast = [3, 2, 4, 5, 6, 2, 3];
+var Humidity_forcast = [22, 30, 25, 45, 50, 51, 40];
+var Icons = [];
